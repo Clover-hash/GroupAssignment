1ab127f6f27862473a5bbc9b3a1a7ee869ad659f
Cloverr <Hokiantoh@gmail.com>
2025-12-18 13:18:29 +0800

    Initial Admin Stuff

--- src/main/java/com/example/groupassignment/HelloApplication.java
+++ src/main/java/com/example/groupassignment/HelloApplication.java
@@ -10,8 +10,7 @@ import java.net.URL;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-        URL fxml = getClass().getResource("AdminLogin.fxml"); // or "/admin/AdminLogin.fxml"
-        System.out.println(fxml); // should not print null
+        URL fxml = getClass().getResource("Admin/AdminLogin.fxml");
         FXMLLoader loader = new FXMLLoader(fxml);
         Scene scene = new Scene(loader.load(), 320, 420);
         stage.setScene(scene);
