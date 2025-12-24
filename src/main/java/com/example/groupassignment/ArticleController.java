69f4830c891e7d6de26d5a2dbcf6dcad57a98dea
Cloverr <Hokiantoh@gmail.com>
2025-12-24 13:07:07 +0800

    Updated KB Controller + Article Controller + New Articles

---
+++ src/main/java/com/example/groupassignment/ArticleController.java
@@ -0,0 +1,19 @@
+package com.example.groupassignment;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
+import java.util.ArrayList;
+
+public class ArticleController {
+
+    @FXML
+    private static TextArea articleText;
+
+    @FXML
+    public static void initialize(String ArticleName) {
+        // change label to read all the data in the article
+        ArrayList<String> Lines;
+
+    }
+}
--- src/main/java/com/example/groupassignment/KBController.java
+++ src/main/java/com/example/groupassignment/KBController.java
@@ -1,41 +1,80 @@
 package com.example.groupassignment;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.ArrayList;
+import java.util.stream.Stream;
 import java.util.Objects;
+
+import static com.example.groupassignment.ArticleController.*;
 
 public class KBController {
     @FXML private TextField searchField;
     @FXML private ListView<String> resultsList;
 
-    // Sample data
-    private final String[] allItems = {
-            "Application for Visa",
-            "How to apply for International Studies",
-            "Scholarship Application",
-            "Job Application Tips",
-            ""
-    };
+    private void ReadArticleTitle(ArrayList<String> allItems) throws IOException {
+        //go through all files in the folder
+        Path folder = Paths.get("C:\\Users\\Hokianto\\Desktop\\Projects\\GroupAssignment\\Knowledgebase");
+        try (Stream<Path> stream = Files.list(folder)){
+            stream.filter(Files::isRegularFile).forEach(path -> {
+                //read the first line
+                try(BufferedReader reader = Files.newBufferedReader(path)){
+                    String Line=reader.readLine();
+                    allItems.add(Line);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        //go next file
+
+        //go till the end
+    }
+    private ArrayList<String> allItems = new ArrayList<String>();
 
     @FXML
-    private void initialize() {
+    private void initialize() throws IOException {
+
         resultsList.setVisible(false);
         resultsList.setManaged(false);
+
+        ReadArticleTitle(allItems);
 
         searchField.textProperty().addListener((obs, old, val) -> applyFilter(val));
         resultsList.setOnMouseClicked(event -> {
             if (event.getClickCount()>=2){
                 String item = resultsList.getSelectionModel().getSelectedItem();
-                openArticle(item);
+                try {
+                    openArticle(item,event);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
-    private void openArticle(String item){
-        System.out.println(item+" Worked");
+    private void openArticle(String item, MouseEvent event) throws IOException {
+        System.out.println(item + " Worked");
+        Parent root = FXMLLoader.load(getClass().getResource("/com/example/groupassignment/KB/ArticleReader.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        //take item as parameter to read the file and pass it to article controller
+        ArticleController.initialize(item);
+        stage.show();
+
     }
+
 
     private void applyFilter(String query) {
         if (query.isEmpty()) {
@@ -43,7 +82,7 @@ public class KBController {
             resultsList.setManaged(false);
         }else {
             String q = query.toLowerCase();
-            resultsList.getItems().setAll(Arrays.stream(allItems).filter(Objects::nonNull).filter(s -> s.toLowerCase().contains(q)).toList());
+            resultsList.getItems().setAll(allItems.stream().filter(Objects::nonNull).filter(s -> s.toLowerCase().contains(q)).toList());
             resultsList.setVisible(true);
             resultsList.setManaged(true);
         }
